f6975cc0ab5642676e6088140d85849699566576
nathanielposanai21-IS <nathanielposanai21@gmail.com>
2026-09-16 12:43:57 +1000

    Updated DiningApp.js

--- AT1_Laboratory_Activities_And_Quiz/Lab3_Dining_Account_Distinction_Extention/DiningApp.js
+++ AT1_Laboratory_Activities_And_Quiz/Lab3_Dining_Account_Distinction_Extention/DiningApp.js
@@ -13,6 +13,8 @@ const { stdin: input, stdout: output } = require("process");
 
 const MealBooking = require("./MealBooking");
 const Student = require("./Student");
+const DiningAccount = require("./DiningAccount");
+const RewardsDiningAccount = require("./RewardsDiningAccount");
 
 const rl = readline.createInterface({ input, output });
 
@@ -33,10 +35,9 @@ async function main() {
             console.log("       DWU DINING MEAL BOOKING");
             console.log("========================================");
             
-            // Ask for student information
-            const studentId = await rl.question("Enter Student ID: ");
-            const firstName = await rl.question("Enter First Name: ");
-            const lastName = await rl.question("Enter Last Name: ");
+                const studentId = await rl.question("Enter Student ID: ");
+                const firstName = await rl.question("Enter First Name: ");
+                const lastName = await rl.question("Enter Last Name: ");
 
             // Create Student Object
             const student = new Student(studentId, firstName, lastName);
--- AT1_Laboratory_Activities_And_Quiz/Lab3_Dining_Account_Distinction_Extention/RewardsDiningAccount.js
+++ AT1_Laboratory_Activities_And_Quiz/Lab3_Dining_Account_Distinction_Extention/RewardsDiningAccount.js
@@ -8,34 +8,34 @@ class RewardsDiningAccount extends DiningAccount {
 
         if (rewardRate < 0 || rewardRate > 1) {
             throw new Error('Reward rate must never be negative.');
-
-            this.#rewardRate = rewardRate;
         }
 
-        calculateReward() {
-            return this.getBalance() * this.#rewardRate / 100;
+        this.#rewardRate = rewardRate;
+    }
+
+    calculateReward() {
+        return this.getBalance() * this.#rewardRate / 100;
+    }
+
+    applyReward() {
+        const reward = this.calculateReward();
+        
+        if (reward > 0) {
+            this.deposit(reward, "Rewards earned");
         }
 
-        applyReward() {
-            const reward = this.calculateReward();
-            
-            if (reward > 0) {
-                this.deposit(reward, "Rewards earned");
-            }
+        return reward;
+    }
 
-            return reward;
-        }
+    getRewardRate() {
+        return this.#rewardRate;
+    }
 
-        getRewardRate() {
-            return this.#rewardRate;
-        }
-
-        displayAccountSummary() {
-            console.log(`Account Number: ${this.getAccountNumber()}`);
-            console.log("Account Type: Rewards Dining Account");
-            console.log('Current Balance: K$' + this.getBalance().toFixed(2));
-            console.log('Reward Rate: ${this.#rewardRate}%');
-        }
+    displayAccountSummary() {
+        console.log(`Account Number: ${this.getAccountNumber()}`);
+        console.log("Account Type: Rewards Dining Account");
+        console.log('Current Balance: K$' + this.getBalance().toFixed(2));
+        console.log(`Reward Rate: ${this.#rewardRate}%`);
     }
 }
 
